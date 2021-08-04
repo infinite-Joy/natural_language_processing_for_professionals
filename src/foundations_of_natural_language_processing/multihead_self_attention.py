bc7be4c3c867259a55b231977286a3afc43bd5a8
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2021-08-04 14:21:25 +0530

    multi head self attention code

--- src/foundations_of_natural_language_processing/multihead_self_attention.py
+++ src/foundations_of_natural_language_processing/multihead_self_attention.py
@@ -30,3 +30,23 @@ print(attention_score)
 attention_score = softmax(attention_score, axis=1) # taking softmax across the rows
 print("attention_score: ")
 print(attention_score)
+
+
+print('Contenating')
+
+import numpy as np
+
+print("Assuming the training on the 8 heads of the attention sub-layer is done.")
+z0h1=np.random.random((3, 64))
+z1h2=np.random.random((3, 64))
+z2h3=np.random.random((3, 64))
+z3h4=np.random.random((3, 64))
+z4h5=np.random.random((3, 64))
+z5h6=np.random.random((3, 64))
+z6h7=np.random.random((3, 64))
+z7h8=np.random.random((3, 64))
+print("shape of one head", z0h1.shape, "dimension of 8 heads", 64*8)
+
+print("Concatenation of heads 1 to 8 to obtain the original 8x64=512 output dimension of the model")
+output_attention=np.hstack((z0h1,z1h2,z2h3,z3h4,z4h5,z5h6,z6h7,z7h8))
+print('final output shape', output_attention.shape)
